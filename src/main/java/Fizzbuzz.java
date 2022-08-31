66b217cb4ca9de290a6e11ded8f3b742e87812bb
leonmaar <myu@gmx.net>
2022-08-31 16:13:00 +0200

    Initial fizzbuzz

---
+++ src/main/java/Fizzbuzz.java
@@ -0,0 +1,21 @@
+public class Fizzbuzz {
+
+    public static void main(String[] args) {
+        System.out.println(fizzbuzz(17));
+    }
+    public static String fizzbuzz(int i){
+        String a;
+
+        if (i % 15 == 0){
+            a = "fizzbuzz";
+        } else if (i % 3 == 0){
+            a = "fizz";
+        } else if (i % 5 == 0){
+            a = "buzz";
+        } else {
+            a = String.valueOf(i);
+        }
+        return a;
+    }
+}
+
